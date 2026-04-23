2248d1b3006ad7efc70a2cfb8236aecf2ad6091c
Alejandro-Murillo20 <jamer153@outlook.com>
2026-04-23 18:47:49 -0500

    Actualizando archivo Hero

--- src/components/organisms/Hero.jsx
+++ src/components/organisms/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="relative h-[500px] w-full bg-neutral-900 overflow-hidden mb-16">
       {/* Imagen de fondo con opacidad para resaltar el texto */}
       <img 
-        src="banner-gala.jpg" 
+        src="/images/banner-gala.jpg" 
         alt="Banner Gala" 
         className="absolute inset-0 w-full h-full object-cover opacity-50"
       />
