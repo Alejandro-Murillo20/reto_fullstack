d0409e3a58155d4af47365e80c5e6e949658e582
Alejandro-Murillo20 <jamer153@outlook.com>
2026-04-23 22:04:45 -0500

    Configurando git pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // 1. Importación de Componentes Globales
 import Navbar from './components/organisms/Navbar';
